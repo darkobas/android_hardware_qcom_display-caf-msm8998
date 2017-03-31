95282627dfc16512e2396118ad46a0607178c666
Naseer Ahmed <naseer@codeaurora.org>
2017-03-31 16:49:05 -0400

    display: Makefile fixes
    * Add dependencies in gpu tonemapper
    * Fix compile for hwc1 and gralloc1
    
    CRs-Fixed: 2027609
    Change-Id: I0f4303d8ea85d93be753e0031bcc3447ba407824

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -21,6 +21,7 @@ LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_HEADER_LIBRARIES        := display_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libqdMetaData libqdutils
 ifneq ($(TARGET_IS_HEADLESS), true)
 LOCAL_SHARED_LIBRARIES        += libGLESv1_CM libdrmutils
@@ -39,6 +40,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libmemalloc
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_HEADER_LIBRARIES        := display_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdutils libdl
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdmemalloc\" -Wno-sign-conversion
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
